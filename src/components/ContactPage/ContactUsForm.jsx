bfb100a07fabb9e80072ddfa7abe56c21d67472e
JatinBawne <jatinbawne2347@gmail.com>
2024-03-08 18:59:14 +0530

    About us added

---
+++ src/components/ContactPage/ContactUsForm.jsx
@@ -0,0 +1,194 @@
+import React, { useEffect, useState } from 'react'
+import {useForm} from 'react-hook-form'
+import { apiConnector } from '../../services/apiconnector';
+import { contactusEndpoint } from '../../services/apis';
+import CountryCode from '../../data/countrycode.json';
+
+
+const ContactUsForm = () => {
+
+    const [loading, setLoading] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState : {errors, isSubmitSuccessful}
+    } = useForm();
+
+
+    const submitContactForm = async(data) => {
+        console.log("Logging Data: ", data);
+
+        try{
+            setLoading(true);
+            // const response = await apiConnector("POST", contactusEndpoint.CONTACT_US_API.data);
+            const response = {status:"OK"};
+            console.log("Logging response: ", response);
+            setLoading(false);
+        }
+
+        catch(error){
+            console.log("Error: ", error.message);
+            setLoading(false);
+        }
+    }
+
+    useEffect( () => {
+        if(isSubmitSuccessful){
+            reset({
+                email : "",
+                firstName : "",
+                lastName : "",
+                message : "",
+                phoneNo : "",
+            })
+        }
+    },[reset, isSubmitSuccessful]);
+
+
+
+  return (
+    <form onSubmit={handleSubmit(submitContactForm)}>
+
+        <div className='flex flex-col gap-14'>
+
+            {/* Name */}
+            <div className='flex gap-5 '>
+                {/* firstName */}
+                <div className='flex flex-col'>
+                    <label htmlFor='firstName'>First Name</label>
+                    <input
+                        type='text'
+                        name='firstName'
+                        id='firstName'
+                        placeholder='Enter First Name'
+                        className='text-black'
+                        {...register('firstName', {required:true})}
+                    />
+                    {
+                        errors.firstName && (
+                            <span>
+                                Please enter your name
+                            </span>
+                        )
+                    }
+                </div>
+
+                {/* lastName */}
+                <div className='flex flex-col'>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input
+                        type='text'
+                        name='lastName'
+                        id='lastName'
+                        placeholder='Enter Last Name'
+                        className='text-black'
+                        {...register('lastName')}
+                    />
+                </div>      
+            </div>
+
+            {/* email   */}
+            <div className='flex flex-col'>
+                <label htmlFor='email'>Email Address</label>
+                <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    placeholder='Enter Email Address'
+                    className='text-black'
+                    {...register('email', {required:true})}
+                />
+                {
+                    errors.email && (
+                        <span>
+                            Please enter your Email Address
+                        </span>
+                    )
+                }
+            </div>
+
+            {/* Phone Number */}
+            <div className='flex flex-col'>
+                <label htmlFor='phonenumber'> Phone Number </label>
+                
+                <div className='flex flex-row gap-1'>
+                    {/* Dropdown */}
+                    <select
+                        name='dropdown'
+                        id='dropdown'
+                        className='bg-yellow-50 w-[80px]'
+                        {...register("countrycode", {required:true})}
+                    >
+                        {
+                            CountryCode.map( (element, index) => {
+                                return (
+                                    <option key={index} value={element.code}>
+                                        {element.code} - {element.country}
+                                    </option>
+                                )
+                            } )
+                        }
+                    </select>
+                
+                    <input
+                        type='number'
+                        name='phonenumber'
+                        id='phonenumber'
+                        placeholder='XXXXX XXXXX'
+                        className='text-black w-[calc(100%-90px)]'
+                        {...register("phoneNo", 
+                        {
+                            required:{value:true, message:"Please Enter Phone Number"},
+                            maxLength:{value:10, message:"Invalid Phone Number"},
+                            minLength:{value:8, message:"Invalid Phone Number"} 
+                        }
+                        )}
+                    />
+                    
+                </div>
+                {
+                    errors.phoneNo && (
+                        <span>
+                            {errors.phoneNo.message}
+                        </span>
+                    )
+                }
+
+            </div>
+
+            {/* message */}
+            <div className='flex flex-col'>
+                <label htmlFor='message'>Message</label>
+                <textarea
+                    name='message'
+                    id='message'
+                    cols={30}
+                    rows={7}
+                    placeholder='Enter Your Message here...'
+                    className='text-black'
+                    {...register("message", {required:true})}
+                />
+                {
+                    errors.message && (
+                        <span>
+                            Please enter your message
+                        </span>
+                    )
+                }
+            </div>
+
+            {/* Button */}
+            <button type='submit'
+                className='rounded-md bg-yellow-50 text-center px-6 text-[16px] font-bold text-black'
+            >
+                Send Message
+            </button>
+
+        </div>
+          
+    </form>
+  )
+}
+
+export default ContactUsForm
