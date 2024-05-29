93de54056c719cb7c6702cace48df18c098d9e1f
JatinBawne <jatinbawne2347@gmail.com>
2024-05-29 23:16:30 +0530

    Dashboard added - PARTIALLY

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/HomePage/Auth/PrivateRoute";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -70,6 +74,22 @@ function App() {
     />
 
     <Route path="/contact" element={<Contact />} />
+
+    <Route
+      element = {
+        <PrivateRoute>
+          <Dashboard/>
+        </PrivateRoute>
+      }
+    >
+    <Route path="dashboard/my-profile" element={<MyProfile/>} />
+    {/* <Route path="dashboard/settings" element={<Setting/>} /> */}
+    </Route>
+
+    
+
+    <Route path="*" element={<Error/>} />
+    
 
     </Routes>
 
